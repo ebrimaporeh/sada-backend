f60ebd5584c73d6800f24e8dcf077b1d621aec8d
ebrimaporeh <coderootsgambia@ebrimaporeh.local>
2026-07-13 20:23:05 +0000

    Use production settings for celery worker

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
 app = Celery('sada')
 app.config_from_object('django.conf:settings', namespace='CELERY')
